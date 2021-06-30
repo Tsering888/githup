e0b2886ddb943c96c87eb8196feaf7abe4f5467b
Tsering <yangduktsang@gmail.com>
2021-06-30 14:16:54 +0200

    add crud

--- crud_php/crud.php
+++ crud_php/crud.php
@@ -6,7 +6,7 @@ function pdo_connect_mysql() {
     $DATABASE_PASS = '';
     $DATABASE_NAME = 'contact';
     try {
-    	return new PDO('mysql.host=' . $DATABASE_HOST='localhost' . $DATABASE_NAME = 'contact', $DATABASE_USER, $DATABASE_PASS);
+    	return new PDO('mysql:host' . $DATABASE_HOST .'localhost' . $DATABASE_NAME .'contact'. $DATABASE_USER . $DATABASE_PASS);
     } catch (PDOException $exception) {
     	// If there is an error with the connection, stop the script and display the error.
     	exit('Failed to connect to database!');
@@ -109,4 +109,163 @@ $contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
 // Get the total number of contacts, this is so we can determine whether there should be a next and previous button
 $num_contacts = $pdo->query('SELECT COUNT(*) FROM contacts')->fetchColumn();
 
+?>
 
+
+<?php
+include 'functions.php';
+$pdo = pdo_connect_mysql();
+$msg = '';
+// Check if POST data is not empty
+if (!empty($_POST)) {
+    // Post data not empty insert a new record
+    // Set-up the variables that are going to be inserted, we must check if the POST variables exist if not we can default them to blank
+    $id = isset($_POST['id']) && !empty($_POST['id']) && $_POST['id'] != 'auto' ? $_POST['id'] : NULL;
+    // Check if POST variable "name" exists, if not default the value to blank, basically the same for all variables
+    $name = isset($_POST['name']) ? $_POST['name'] : '';
+    $email = isset($_POST['email']) ? $_POST['email'] : '';
+    $phone = isset($_POST['phone']) ? $_POST['phone'] : '';
+    $title = isset($_POST['title']) ? $_POST['title'] : '';
+    $created = isset($_POST['created']) ? $_POST['created'] : date('Y-m-d H:i:s');
+    // Insert new record into the contacts table
+    $stmt = $pdo->prepare('INSERT INTO contacts VALUES (?, ?, ?, ?, ?, ?)');
+    $stmt->execute([$id, $name, $email, $phone, $title, $created]);
+    // Output message
+    $msg = 'Created Successfully!';
+}
+?>
+
+
+<?=template_header('Create')?>
+
+<div class="content update">
+	<h2>Create Contact</h2>
+    <form action="create.php" method="post">
+        <label for="id">ID</label>
+        <label for="name">Name</label>
+        <input type="text" name="id" placeholder="26" value="auto" id="id">
+        <input type="text" name="name" placeholder="John Doe" id="name">
+        <label for="email">Email</label>
+        <label for="phone">Phone</label>
+        <input type="text" name="email" placeholder="johndoe@example.com" id="email">
+        <input type="text" name="phone" placeholder="2025550143" id="phone">
+        <label for="title">Title</label>
+        <label for="created">Created</label>
+        <input type="text" name="title" placeholder="Employee" id="title">
+        <input type="datetime-local" name="created" value="<?=date('Y-m-d\TH:i')?>" id="created">
+        <input type="submit" value="Create">
+    </form>
+    <?php if ($msg): ?>
+    <p><?=$msg?></p>
+    <?php endif; ?>
+</div>
+
+<?=template_footer()?>
+
+<?php
+include 'functions.php';
+$pdo = pdo_connect_mysql();
+$msg = '';
+// Check if the contact id exists, for example update.php?id=1 will get the contact with the id of 1
+if (isset($_GET['id'])) {
+    if (!empty($_POST)) {
+        // This part is similar to the create.php, but instead we update a record and not insert
+        $id = isset($_POST['id']) ? $_POST['id'] : NULL;
+        $name = isset($_POST['name']) ? $_POST['name'] : '';
+        $email = isset($_POST['email']) ? $_POST['email'] : '';
+        $phone = isset($_POST['phone']) ? $_POST['phone'] : '';
+        $title = isset($_POST['title']) ? $_POST['title'] : '';
+        $created = isset($_POST['created']) ? $_POST['created'] : date('Y-m-d H:i:s');
+        // Update the record
+        $stmt = $pdo->prepare('UPDATE contacts SET id = ?, name = ?, email = ?, phone = ?, title = ?, created = ? WHERE id = ?');
+        $stmt->execute([$id, $name, $email, $phone, $title, $created, $_GET['id']]);
+        $msg = 'Updated Successfully!';
+    }
+    // Get the contact from the contacts table
+    $stmt = $pdo->prepare('SELECT * FROM contacts WHERE id = ?');
+    $stmt->execute([$_GET['id']]);
+    $contact = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$contact) {
+        exit('Contact doesn\'t exist with that ID!');
+    }
+} else {
+    exit('No ID specified!');
+}
+?>
+
+<?=template_header('Read')?>
+
+<div class="content update">
+	<h2>Update Contact #<?=$contact['id']?></h2>
+    <form action="update.php?id=<?=$contact['id']?>" method="post">
+        <label for="id">ID</label>
+        <label for="name">Name</label>
+        <input type="text" name="id" placeholder="1" value="<?=$contact['id']?>" id="id">
+        <input type="text" name="name" placeholder="John Doe" value="<?=$contact['name']?>" id="name">
+        <label for="email">Email</label>
+        <label for="phone">Phone</label>
+        <input type="text" name="email" placeholder="johndoe@example.com" value="<?=$contact['email']?>" id="email">
+        <input type="text" name="phone" placeholder="2025550143" value="<?=$contact['phone']?>" id="phone">
+        <label for="title">Title</label>
+        <label for="created">Created</label>
+        <input type="text" name="title" placeholder="Employee" value="<?=$contact['title']?>" id="title">
+        <input type="datetime-local" name="created" value="<?=date('Y-m-d\TH:i', strtotime($contact['created']))?>" id="created">
+        <input type="submit" value="Update">
+    </form>
+    <?php if ($msg): ?>
+    <p><?=$msg?></p>
+    <?php endif; ?>
+</div>
+
+<?=template_footer()?>
+
+
+
+<?php
+include 'functions.php';
+$pdo = pdo_connect_mysql();
+$msg = '';
+// Check that the contact ID exists
+if (isset($_GET['id'])) {
+    // Select the record that is going to be deleted
+    $stmt = $pdo->prepare('SELECT * FROM contacts WHERE id = ?');
+    $stmt->execute([$_GET['id']]);
+    $contact = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$contact) {
+        exit('Contact doesn\'t exist with that ID!');
+    }
+    // Make sure the user confirms beore deletion
+    if (isset($_GET['confirm'])) {
+        if ($_GET['confirm'] == 'yes') {
+            // User clicked the "Yes" button, delete record
+            $stmt = $pdo->prepare('DELETE FROM contacts WHERE id = ?');
+            $stmt->execute([$_GET['id']]);
+            $msg = 'You have deleted the contact!';
+        } else {
+            // User clicked the "No" button, redirect them back to the read page
+            header('Location: read.php');
+            exit;
+        }
+    }
+} else {
+    exit('No ID specified!');
+}
+?>
+
+
+<?=template_header('Delete')?>
+
+<div class="content delete">
+	<h2>Delete Contact #<?=$contact['id']?></h2>
+    <?php if ($msg): ?>
+    <p><?=$msg?></p>
+    <?php else: ?>
+	<p>Are you sure you want to delete contact #<?=$contact['id']?>?</p>
+    <div class="yesno">
+        <a href="delete.php?id=<?=$contact['id']?>&confirm=yes">Yes</a>
+        <a href="delete.php?id=<?=$contact['id']?>&confirm=no">No</a>
+    </div>
+    <?php endif; ?>
+</div>
+
+<?=template_footer()?>
