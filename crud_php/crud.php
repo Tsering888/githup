4bf1994eb7e1cc644bed1db888ce345a164f1baf
Tsering <yangduktsang@gmail.com>
2021-06-30 13:17:39 +0200

    t

---
+++ crud_php/crud.php
@@ -0,0 +1,112 @@
+
+<?php
+function pdo_connect_mysql() {
+    $DATABASE_HOST = 'localhost';
+    $DATABASE_USER = 'root';
+    $DATABASE_PASS = '';
+    $DATABASE_NAME = 'contact';
+    try {
+    	return new PDO('mysql.host=' . $DATABASE_HOST='localhost' . $DATABASE_NAME = 'contact', $DATABASE_USER, $DATABASE_PASS);
+    } catch (PDOException $exception) {
+    	// If there is an error with the connection, stop the script and display the error.
+    	exit('Failed to connect to database!');
+    }
+}
+function template_header($title) {
+echo <<<EOT
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>$title</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+	</head>
+	<body>
+    <nav class="navtop">
+    	<div>
+    		<h1>Website Title</h1>
+            <a href="index.php"><i class="fas fa-home"></i>Home</a>
+    		<a href="read.php"><i class="fas fa-address-book"></i>Contacts</a>
+    	</div>
+    </nav>
+
+
+
+EOT;
+}
+function template_footer() {
+echo <<<EOT
+    
+</body>
+</html>
+
+EOT;
+}
+
+?>
+
+<?=template_header('Home')?>
+
+
+<div class="content read">
+	<h2>Read Contacts</h2>
+	<a href="create.php" class="create-contact">Create Contact</a>
+	<table>
+        <thead>
+            <tr>
+                <td>#</td>
+                <td>Name</td>
+                <td>Email</td>
+                <td>Phone</td>
+                <td>Title</td>
+                <td>Created</td>
+                <td></td>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($contacts as $contact): ?>
+            <tr>
+                <td><?=$contact['id']?></td>
+                <td><?=$contact['name']?></td>
+                <td><?=$contact['email']?></td>
+                <td><?=$contact['phone']?></td>
+                <td><?=$contact['title']?></td>
+                <td><?=$contact['created']?></td>
+                <?php endforeach; ?>
+                <td class="actions">
+                    <a href="update.php?id=<?=$contact['id']?>" class="edit"><i class="fas fa-pen fa-xs"></i></a>
+                    <a href="delete.php?id=<?=$contact['id']?>" class="trash"><i class="fas fa-trash fa-xs"></i></a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+	<div class="pagination">
+		
+		<a href="read.php?page=<?=$page-1?>"><i class="fas fa-angle-double-left fa-sm"></i></a>
+		
+		<a href="read.php?page=<?=$page+1?>"><i class="fas fa-angle-double-right fa-sm"></i></a>
+		
+</div>
+
+<?=template_footer()?>
+
+<?php
+// Connect to MySQL database
+$pdo = pdo_connect_mysql();
+// Get the page via GET request (URL param: page), if non exists default the page to 1
+$page = isset($_GET['page']) && is_numeric($_GET['page']) ? (int)$_GET['page'] : 1;
+// Number of records to show on each page
+$records_per_page = 5;
+
+$stmt = $pdo->prepare('SELECT * FROM contacts ORDER BY id LIMIT :current_page, :record_per_page');
+$stmt->bindValue(':current_page', ($page-1)*$records_per_page, PDO::PARAM_INT);
+$stmt->bindValue(':record_per_page', $records_per_page, PDO::PARAM_INT);
+$stmt->execute();
+// Fetch the records so we can display them in our template.
+$contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Get the total number of contacts, this is so we can determine whether there should be a next and previous button
+$num_contacts = $pdo->query('SELECT COUNT(*) FROM contacts')->fetchColumn();
+
+
